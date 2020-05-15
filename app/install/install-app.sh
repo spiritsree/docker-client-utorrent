7c568c9bd348bb2a29ebb0bfeab9bb9184352722
Sreejith G <spirit.sree@gmail.com>
2020-05-15 18:54:00 +1000

    improve install

--- app/install/install-app.sh
+++ app/install/install-app.sh
@@ -6,6 +6,8 @@ DOCKERIZE_IMAGE_URL="https://github.com/jwilder/dockerize/releases/download/v${D
 UTSERVER_URL="http://download.ap.bittorrent.com/track/beta/endpoint/utserver/os/linux-x64-ubuntu-13-04"
 NG_UI="https://github.com/psychowood/ng-torrent-ui/releases/latest/download/webui.zip"
 TMP_DIR="/opt/setup-dir"
+LIB_SSL_URL="http://archive.ubuntu.com/ubuntu/pool/main/o/openssl1.0"
+LIB_SSL="libssl1.0.0_1.0.2n-1ubuntu5.3_amd64.deb"
 
 mkdir -p /utorrent/webui/{default,ng}
 mkdir -p "${TMP_DIR}"
@@ -23,6 +25,11 @@ mv /utorrent/webui.zip /utorrent/webui/default/
 
 echo "**** install custom UI ****"
 curl -sSL "${NG_UI}" -o /utorrent/webui/ng/webui.zip
+
+if [[ $(lsb_release -rs | awk -F'.' '{ print $1 }') -gt 18 ]]; then
+    curl -sSL "${LIB_SSL_URL}/${LIB_SSL}" -o "${LIB_SSL}"
+    apt-get install "./${LIB_SSL}" > /dev/null
+fi
 
 popd > /dev/null || exit 1
 rm -rf "${TMP_DIR}"
